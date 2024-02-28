ee16dfa1d44dff00ebc0a4eb6f73654347de92df
Soribel S.B <soribelsantos0514@gmail.com>
2024-02-28 06:57:51 -0400

    Edit PhoneBook component to add important btn

--- src/part2/2.1/PhoneBook.tsx
+++ src/part2/2.1/PhoneBook.tsx
@@ -2,14 +2,18 @@ import { useState,useEffect } from "react";
 import axios from "axios"
 
 
+interface IProps{
+  labelBtn : string
+}
 interface IPhoneBook{
     name: string,
-    number: string
+    number: string,
+    important: boolean
 }
 
-function PhoneBook() {
+function PhoneBook({labelBtn}:IProps) {
     
-    const [persons, setPersons] = useState([{name: "Arto Hellas", number: "12345"}]); 
+    const [persons, setPersons] = useState([]); 
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
     const [filter, setFilter] = useState("");
@@ -27,6 +31,7 @@ function PhoneBook() {
         const phoneBookObj: IPhoneBook = {
           name: newName,
           number: newNumber,
+          important: Math.random() < 0.5
         }
         const isExist = persons.find((element) => element.name === newName || element.number === newNumber)
     
@@ -62,46 +67,57 @@ function PhoneBook() {
     }
     const filteredPersons = filter ? persons.find((element) => element.name === filter) : persons
 
-    let label;
-    function toggleImportance(){
-        label = persons.map(person => person.important)
-        label  ? 'make not important' : 'make important';
+
+    function toggleImportance(id:string){
+      const url = `http://localhost:3001/persons/${id}`;
+      const person = persons.find(p => p.id === id);
+      const changedPerson = { ...person, important: !person.important };
+      labelBtn = person.important ? 'make not important': 'make important';
+    
+      axios.put(url, changedPerson).then(response => {
+        setPersons(persons.map(p => p.id !== id ? p : response.data))
+      })
     }
+
   return <>
-      <div className="container p-3 text-center">
-    <h2>Phonebook</h2>
-    <form onSubmit={Phonefilter} className="my-4">
-      Filter: <input type="text" value={filter} onChange={handlePhoneFilterChange} placeholder="Insert a name"/> 
-    </form>
-    <div>
-      {filteredPersons && <p key={filteredPersons.number}> {filteredPersons.name} {filteredPersons.number}</p>}
-    </div>
 
-    <h4>Add a new contact</h4>
-    <form  onSubmit={addPhoneBook}>
-        <div>
-          <div className="m-2">
-            Name: <input value={newName} onChange={handlePhoneNameChange}/>
-          </div>
-          <div className="m-2">
-            Number: <input type="tel" value={newNumber} onChange={handlePhoneNumberChange}/>
-          </div>
-        </div>
+    <div className="container p-3 text-center">
+      <h2>Phonebook</h2>
+      <form onSubmit={Phonefilter} className="my-4">
+        Filter: <input type="text" value={filter} onChange={handlePhoneFilterChange} placeholder="Insert a name"/> 
+      </form>
       <div>
-        <button className="m-2 p-2 btn btn-primary" type="submit">Add</button>
+        {filteredPersons && <p key={filteredPersons.id}> {filteredPersons.name} {filteredPersons.number}</p>}
       </div>
-    </form>
 
-    <h2>Numbers</h2>
-    <div className="container d-flex justify-content-center aling-items-center">
-      <ul className="list-group list-group-flush">
-          {persons.map(person => <>
-            <li key={person.number} className="list-group-item"> {person.name} {person.number}</li>
-            <button onClick={toggleImportance}>{label}</button>
-          </>
-          )}
-      </ul>
-    </div>
+      <h4>Add a new contact</h4>
+      <form  onSubmit={addPhoneBook}>
+          <div>
+            <div className="m-2">
+              Name: <input value={newName} onChange={handlePhoneNameChange}/>
+            </div>
+            <div className="m-2">
+              Number: <input type="tel" value={newNumber} onChange={handlePhoneNumberChange}/>
+            </div>
+          </div>
+        <div>
+          <button className="m-2 p-2 btn btn-primary" type="submit">Add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <div className="container d-flex justify-content-center aling-items-center">
+        <ul className="list-group list-group-flush">
+            {persons.map(person =>
+                <li key={person.id} className="list-group-item">
+                {person.name} {person.number}
+                <button className= "m-1 btn btn-success" onClick={() => toggleImportance(person.id)}>
+                  {person.important ? "Make Not Important" : "Make Important"}
+                </button>
+                </li>
+            )}
+        </ul>
+      </div>
   </div>
   </>
 }
